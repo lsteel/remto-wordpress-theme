d93eb70b42dc7a4b283f83900e118c0ab375d559
lsteel <lorinsteel@gmail.com>
2016-04-27 09:08:16 -0600

    included social icons and london background for footer. Skeletonized page.php

--- page.php
+++ page.php
@@ -17,18 +17,26 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-			<?php
-			while ( have_posts() ) : the_post();
+			<div class="container">
+				<div class="row">
+					<div class="twelve columns">
 
-				get_template_part( 'template-parts/content', 'page' );
+						<?php
+						while ( have_posts() ) : the_post();
 
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
+							get_template_part( 'template-parts/content', 'page' );
 
-			endwhile; // End of the loop.
-			?>
+							// If comments are open or we have at least one comment, load up the comment template.
+							if ( comments_open() || get_comments_number() ) :
+								comments_template();
+							endif;
+
+						endwhile; // End of the loop.
+						?>
+
+					</div>
+				</div>
+			</div>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
